e86bfb8ce1c80aed49b3109c0a4fc0b52f14429b
Nathan Michlo <NathanJMichlo@gmail.com>
2019-11-19 11:22:35 +0000

    changes

--- run_jupyter_notebook.sh
+++ run_jupyter_notebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # need to specify ip of computer, otherwise cant access jupyter remotely...
-jupyter-notebook --ip "$(hostname -I | awk '{print $1}')"
+jupyter-notebook --ip "$(hostname -I | awk '{print $1}')" --port 8080
